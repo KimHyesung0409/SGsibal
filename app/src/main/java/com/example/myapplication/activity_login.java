888a9f9295a8f1df155f162f82303a70910adfe2
Bae-sucheol <81005707+Bae-sucheol@users.noreply.github.com>
2021-04-19 16:36:27 +0900

    5555555
    5555555555

--- app/src/main/java/com/example/myapplication/activity_login.java
+++ app/src/main/java/com/example/myapplication/activity_login.java
@@ -127,6 +127,7 @@ public class activity_login extends AppCompatActivity implements GoogleApiClient
 
     public void onClickLogin(View view)
     {
+        /*
         email = editText_email.getText().toString().trim();
         password = editText_pw.getText().toString().trim();
         VerifyString verifyString = new VerifyString();
@@ -134,6 +135,10 @@ public class activity_login extends AppCompatActivity implements GoogleApiClient
         if(verifyString.isValidEmail(email) && verifyString.isValidPasswd(password)) {
             loginUser(email, password);
         }
+        */
+        Intent intent = new Intent(activity_login.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void onClickSignUp(View view)

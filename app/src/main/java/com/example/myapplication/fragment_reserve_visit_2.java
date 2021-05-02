680902db6176bc67cf7dd601d9a419372b3cb4fc
KimHyesung0409 <81004569+KimHyesung0409@users.noreply.github.com>
2021-05-02 13:52:39 +0900

    예약날짜시간
    방문 예약 2단계에서 캘린더뷰랑 타임피커 원하는 날짜시간선택하고 버튼클릭시 예약날짜 출력 - 이거를 유저(고객,시터) 예약정보에 예약시간으로 데이터 저장되어야 함.

--- app/src/main/java/com/example/myapplication/fragment_reserve_visit_2.java
+++ app/src/main/java/com/example/myapplication/fragment_reserve_visit_2.java
@@ -4,18 +4,82 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class fragment_reserve_visit_2 extends Fragment {
 
     ViewGroup viewGroup;
+    CalendarView calendarView;
+    TimePicker time_picker;
+    TextView reserve_visit_year,reserve_visit_month, reserve_visit_day, reserve_visit_hour, reserve_visit_minute;
+    String reserve_content;
+    int selectYear, selectMonth, selectDay, selectHour, selectMinute;
+    Button check_reserveTime;
+    LinearLayout reserveTime;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         viewGroup = (ViewGroup)inflater.inflate(R.layout.fragment_reserve_visit_2, container, false);
+
+        calendarView = (CalendarView)viewGroup.findViewById(R.id.calendarView);
+        time_picker = (TimePicker)viewGroup.findViewById(R.id.time_picker);
+        reserve_visit_year = (TextView)viewGroup.findViewById(R.id.reserve_visit_year);
+        reserve_visit_month = (TextView)viewGroup.findViewById(R.id.reserve_visit_month);
+        reserve_visit_day = (TextView)viewGroup.findViewById(R.id.reserve_visit_day);
+        reserve_visit_hour = (TextView)viewGroup.findViewById(R.id.reserve_visit_hour);
+        reserve_visit_minute = (TextView)viewGroup.findViewById(R.id.reserve_visit_minute);
+        check_reserveTime = (Button)viewGroup.findViewById(R.id.check_reserveTime);
+        reserveTime = (LinearLayout)viewGroup.findViewById(R.id.reserveTime);
+
+
+        reserveTime.setVisibility(View.INVISIBLE);
+
+        check_reserveTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reserveTime.setVisibility(View.VISIBLE);
+
+                reserve_visit_year.setText(Integer.toString(selectYear));
+                reserve_visit_month.setText(Integer.toString(selectMonth));
+                reserve_visit_day.setText(Integer.toString(selectDay));
+                reserve_visit_hour.setText(Integer.toString(selectHour));
+                reserve_visit_minute.setText(Integer.toString(selectMinute));
+
+                reserve_content = reserve_visit_year.getText().toString()+"년"+reserve_visit_month.getText().toString()+"월"
+                        +reserve_visit_day.getText().toString()+"일"+reserve_visit_hour.getText().toString()+"시"
+                        +reserve_visit_minute.getText().toString()+"분";
+
+            }
+        });
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfmonth) {
+
+                selectYear = year;
+                selectMonth = month + 1;
+                selectDay = dayOfmonth;
+
+            }
+        });
+
+        time_picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+                selectHour = hour;
+                selectMinute = minute;
+            }
+        });
+
+
 
         return viewGroup;
     }

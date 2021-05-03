63afaceb4cf65975741280d5eeaca378234d1960
u-naul <81006992+u-naul@users.noreply.github.com>
2021-05-03 14:07:34 +0900

    123

---
+++ app/src/main/java/com/example/myapplication/service_center.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import android.view.View;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class service_center extends Fragment implements View.OnClickListener{
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.service_center);
+    }
+
+}
